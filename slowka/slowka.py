9fdd00fe8957edce46b735b81480f4e0ebd15f40
Piotr <piotr_czajka@outlook.com>
2017-05-21 18:07:34 +0200

    English unit 5 vocabulary lesson added

---
+++ slowka/slowka.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys
+
+words = []
+
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        en, pl = line.split(':')
+        words.append((en.strip(), pl.strip()))
+
+x = input('pl->en(1) or en->pl(2)? ')
+
+while True:
+    for en, pl in words:
+        to_guess = en if x == '1' else pl
+        word = pl if to_guess is en else pl
+        answer = input(word + ': ')
+        if answer in to_guess:
+            print('Correct!')
+        print(to_guess)
