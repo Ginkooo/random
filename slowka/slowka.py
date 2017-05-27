bdfab328a8f2def2e0eaf946de87ce27e9dec891
Piotr Czajka <piotr_czajka@outlook.com>
2017-05-27 22:30:48 +0200

    dfdf

--- slowka/slowka.py
+++ slowka/slowka.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python
 import sys
+import codecs
 
-if len(sys.argv) == 1:
-    print("usage: ./slowka.py FILENAME")
-    quit()
+def main():
+    if len(sys.argv) == 1:
+        print("usage: ./slowka.py FILENAME")
+        quit()
 
-words = []
+    words = []
 
-with open(sys.argv[1], 'r') as f:
-    for line in f:
-        en, pl = line.split(':')
-        words.append((en.strip(), pl.strip()))
+    with codecs.open(sys.argv[1], 'r', 'utf-8') as f:
+        for line in f:
+            en, pl = line.split(':')
+            words.append((en.strip(), pl.strip()))
 
-x = input('pl->en(1) or en->pl(2)? ')
+    x = input('pl->en(1) or en->pl(2)? ')
 
-while True:
-    for en, pl in words:
-        to_guess = en if x == '1' else pl
-        word = pl if x=='1' else en
-        answer = input(word + ': ')
-        if answer in to_guess:
-            print('Correct! - ', end='')
-        print(to_guess)
+    while True:
+            for en, pl in words:
+                to_guess = en if x == '1' else pl
+                word = pl if x=='1' else en
+                try:
+                    answer = input(word + ': ')
+                except UnicodeError:
+                    answer = input(word.encode('ascii', 'replace').decode('ascii') + ': ')
+                if answer in to_guess:
+                    print('Correct! - ', end='')
+                try:
+                    print(to_guess)
+                except UnicodeError:
+                    print(to_guess.encode('ascii', 'replace').decode('ascii'))
+
+if __name__ == '__main__':
+    main()
+
